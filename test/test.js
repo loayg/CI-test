423d5866644db41be8f99c4f6cc3b895128e3ed2
loay <loay.gewily@waltzapp.com>
2017-10-31 15:29:28 -0400

    2nd commit

---
+++ test/test.js
@@ -0,0 +1,14 @@
+'use strict';
+var expect = require('chai').expect;
+
+describe("Simple Math tests", function() {
+  describe("two equal numbers", function() {
+    it("expects 1 to equal 2", function() {
+    expect(1).to.eql(1);
+    });
+
+    it("converts the basic colors", function() {
+      expect(1).to.eql(2);
+    });
+  });
+});
